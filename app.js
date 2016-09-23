ab652a2166d87093db876f6b14d356de16161a4e
Jacquelyn <jtorborg1227@comcast.net>
2016-09-23 17:28:05 -0500

    some functionality but messy

--- app.js
+++ app.js
@@ -9,26 +9,53 @@ $(document).ready(function () {
 
     fields.forEach(function (element, index) {
       employee[element.name] = element.value;
+
     });
 
     console.log('employee object', employee);
 
+var empSal = parseInt(employee.employeeSalary);
+console.log(empSal);
+var monthlySalary = Math.round(empSal/12);
+console.log(monthlySalary);
+
+
+$("#employeeMonthlySalary").append(monthlySalary);
+
+
+
+
     // clear form data
     $('#employeeInfo').find('input[type=text]').val('');
-    $('#employeeInfo').find('input[type=text]').val('');
+    $('#employeeInfo').find('input[type=number]').val('');
 
 
+    //monthly salary container
+    //$(#employeeMonthlySalary).append();
     // appending to the DOM
     appendDom(employee);
   });
 
+//store object in array
+// var storeEmployee = [];
+//
+// storeEmployee.push(employee);
+// console.log('store employee', storeEmployee);
 
 
-  function appendDom(emp) {
+
+  function appendDom(empInfo) {
     var $emp = $('<div class="employee"></div>');
 
-    $emp.append('<p>' + emp.employeeFirstName + ' ' + emp.employeeLastName + '</p>');
+    $emp.append('<p>' + empInfo.employeeFirstName + ' ' + empInfo.employeeLastName + '</p>');
+    $emp.append('<p>' + empInfo.employeeIdNumber + '</p>');
+    $emp.append('<p>' + empInfo.employeeJobTitle + '</p>');
+
+    $emp.append('<p>' + empInfo.employeeSalary + '</p>');
 
     $('#employeeData').append($emp);
   }
 });
+
+
+//select part of table that corresponding to table and
